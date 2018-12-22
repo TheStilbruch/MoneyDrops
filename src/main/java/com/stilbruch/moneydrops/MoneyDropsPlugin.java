b661ca8d9ad535e774f0f3a2953d5c294e8fdff9
Hayden Pope <stilbruch39@gmail.com>
2018-12-22 16:53:45 -0600

    initial commit

---
+++ src/main/java/com/stilbruch/moneydrops/MoneyDropsPlugin.java
@@ -0,0 +1,11 @@
+package com.stilbruch.moneydrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MoneyDropsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("DING");
+    }
+}
